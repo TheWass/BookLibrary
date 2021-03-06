13c8b335012ecc3f59587231c652b3587aabadcf
TheWass <lord.d35ruct0@gmail.com>
2021-03-05 22:13:44 -0500

    Implemented barcode scanner.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { connect, Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
@@ -12,54 +12,56 @@ import { StatusBar } from 'expo-status-bar';
 import { ReduxStore } from './redux/store';
 import BooksPage from './pages/books';
 import AddButton from './components/addButton';
+import BarcodeScanner from './pages/barcodeScanner';
 
-const Stack = createStackNavigator();
+const MainStack = createStackNavigator();
+const RootStack = createStackNavigator();
 
-interface ReactState {
-    isReady: boolean;
-}
+function MainStackScreen() {
+    return (<MainStack.Navigator>
+        <RootStack.Screen 
+            name="Books"
+            component={BooksPage}
+            options={{
+                headerRight: function headerRight() { return <AddButton />; }
+            }}
+        />
+    </MainStack.Navigator>);
+  }
 
-class App extends React.Component<{}, ReactState>  {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            isReady: false,
-        };
-    }
+
+export default function App(): JSX.Element {
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        const timeout = setTimeout(async () => {
+            await Font.loadAsync({
+                Roboto: require('native-base/Fonts/Roboto.ttf'),
+                Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+                ...Ionicons.font,
+            });
+            setIsLoading(false);
+        }, 1000)
   
-    async componentDidMount() {
-        await Font.loadAsync({
-            Roboto: require('native-base/Fonts/Roboto.ttf'),
-            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-            ...Ionicons.font,
-        });
-        this.setState({ isReady: true });
-    }
+        return () => clearTimeout(timeout);
+    }, [])
   
-    render() {
-        if (!this.state.isReady) {
-            return <AppLoading />;
-        }
-    
-        return (<Root>
-            <Provider store={ ReduxStore.getStore() }>
-                <StatusBar style="auto" />
-                <NavigationContainer>
-                    <Stack.Navigator>
-                        <Stack.Screen 
-                            name="Books"
-                            component={BooksPage}
-                            options={{
-                                headerRight: AddButton
-                            }}
-                        />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </Provider>
-        </Root>);
+    if (isLoading) {
+        return <AppLoading />
     }
+    return (<Root>
+        <Provider store={ ReduxStore.getStore() }>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                <RootStack.Navigator>
+                    <RootStack.Screen
+                        name="Main"
+                        component={MainStackScreen}
+                        options={{ headerShown: false }}
+                    />
+                    <RootStack.Screen name="Scan ISBN" component={BarcodeScanner} />
+                </RootStack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    </Root>);
 }
-export default connect(
-    null,
-    null
-)(App);
--- components/addButton.tsx
+++ components/addButton.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native'
+import { ActionSheetIOS } from 'react-native';
 import { connect } from 'react-redux';
 import { Button, Icon } from 'native-base';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const onPress = (navigation: any) => {
+    ActionSheetIOS.showActionSheetWithOptions(
+        {
+            options: ["Cancel", "Scan ISBN", "Manual Entry"],
+            cancelButtonIndex: 0,
+        },
+        (buttonIndex) => {
+            switch(buttonIndex) {
+                case 0: //Cancel
+                    break;
+                case 1: //Scan
+                    navigation.navigate('Scan ISBN')
+                    break;
+                case 2: //Enter
+
+                    break;
+            }
+        }
+    );
+}
+
 const AddButton = () => {
+    const navigation = useNavigation();
     return (
-        <Button transparent>
+        <Button transparent onPress={() => onPress(navigation)}>
             <Icon name='add' />
         </Button>
     );
