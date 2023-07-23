e585b189f56a8ad626d5ac45dcc543d2a3000847
TheWass <lord.d35ruct0@gmail.com>
2023-07-23 11:47:51 -0400

    removed console error which was causing issues.

--- src/pages/entryForm.tsx
+++ src/pages/entryForm.tsx
@@ -47,7 +47,6 @@ const EntryForm = ({ navigation, route }: Props) => {
             if (typeof ex == 'string'){
                 setErrorMsg(ex);
             }
-            console.error(ex);
         }
     };
 
